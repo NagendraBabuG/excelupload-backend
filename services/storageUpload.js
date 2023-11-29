3f2b7bc8e1ae368ee3103df65b82936655e99719
Nagendra <gnagendra0009@gmail.com>
2023-11-29 18:47:13 +0530

    initial commit

--- services/storageUpload.js
+++ services/storageUpload.js
@@ -1,5 +1,15 @@
 const multer = require('multer')
-const upload = multer({dest : 'uploads/'})
+const storage = multer.diskStorage(
+    {
+        destination: (req, file, cb) =>{
+            cb(null, "./uploads")
+        },
+        filename: (req, file, cb) => {
+            cb(null, file.originalname)
+        }
+    }
+)
+const upload = multer({storage : storage})
 
 
 module.exports = upload
